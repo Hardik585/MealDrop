7a31ddf77dd04f2d6df35a375ca11b8b2cc21523
Hardik585 <Hardikkaushik83126@gmail.com>
2026-03-19 10:04:37 +0530

    change here also tailwind to bootstrap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,30 @@
-import { useState } from 'react'
-import './App.css'
+import './App.css';
 
-import '@mantine/core/styles.css';
-import { MantineProvider } from '@mantine/core';
-import Demo from './demo/demo';
+import {Route, Routes} from 'react-router-dom';
+
+import Menubar from './components/Menubar/Menubar.component';
+
+import Home from './pages/Home/Home';
+import Contact from './pages/Contact/Contact';
+import ExploreFood from './pages/ExploreFood/ExploreFood';
+import FoodDetails from './pages/Food Details/FoodDetails';
+import { ToastContainer } from 'react-toastify';
 
 
 function App() {
 
   return (
-       <MantineProvider>
-        <Demo/>
-       </MantineProvider>
+    <div>
+      <Menubar />
+      <ToastContainer/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/explore' element={<ExploreFood/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/food/:id' element={<FoodDetails/>}/>
+      </Routes>
+    </div>
+
   )
 }
 
